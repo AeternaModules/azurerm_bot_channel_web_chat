464c8257e41cf23ac760855d80f39acae09ac088
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:33:29 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "bot_channel_web_chats" {
+  description = <<EOT
+Map of bot_channel_web_chats, attributes below
+Required:
+    - bot_name
+    - location
+    - resource_group_name
+Optional:
+    - site (block):
+        - endpoint_parameters_enabled (optional)
+        - name (required)
+        - storage_enabled (optional)
+        - user_upload_enabled (optional)
+EOT
+
+  type = map(object({
+    bot_name            = string
+    location            = string
+    resource_group_name = string
+    site = optional(object({
+      endpoint_parameters_enabled = optional(bool)
+      name                        = string
+      storage_enabled             = optional(bool, true)
+      user_upload_enabled         = optional(bool, true)
+    }))
+  }))
+}
+
