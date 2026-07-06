b6ef63c219c2a47dbfb6899198c4230bc03befb9
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "bot_channel_web_chats" {
   description = "All bot_channel_web_chat resources"
   value       = azurerm_bot_channel_web_chat.bot_channel_web_chats
 }
+output "bot_channel_web_chats_bot_name" {
+  description = "List of bot_name values across all bot_channel_web_chats"
+  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.bot_name]
+}
+output "bot_channel_web_chats_location" {
+  description = "List of location values across all bot_channel_web_chats"
+  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.location]
+}
+output "bot_channel_web_chats_resource_group_name" {
+  description = "List of resource_group_name values across all bot_channel_web_chats"
+  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.resource_group_name]
+}
+output "bot_channel_web_chats_site" {
+  description = "List of site values across all bot_channel_web_chats"
+  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.site]
+}
 
