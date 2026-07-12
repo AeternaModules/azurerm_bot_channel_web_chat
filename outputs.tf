3ca6841df2297ff3274d4e00571f8d5e8e1f65a4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "bot_channel_web_chats" {
-  description = "All bot_channel_web_chat resources"
-  value       = azurerm_bot_channel_web_chat.bot_channel_web_chats
-}
 output "bot_channel_web_chats_bot_name" {
-  description = "List of bot_name values across all bot_channel_web_chats"
-  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.bot_name]
+  description = "Map of bot_name values across all bot_channel_web_chats, keyed the same as var.bot_channel_web_chats"
+  value       = { for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : k => v.bot_name }
 }
 output "bot_channel_web_chats_location" {
-  description = "List of location values across all bot_channel_web_chats"
-  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.location]
+  description = "Map of location values across all bot_channel_web_chats, keyed the same as var.bot_channel_web_chats"
+  value       = { for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : k => v.location }
 }
 output "bot_channel_web_chats_resource_group_name" {
-  description = "List of resource_group_name values across all bot_channel_web_chats"
-  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.resource_group_name]
+  description = "Map of resource_group_name values across all bot_channel_web_chats, keyed the same as var.bot_channel_web_chats"
+  value       = { for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : k => v.resource_group_name }
 }
 output "bot_channel_web_chats_site" {
-  description = "List of site values across all bot_channel_web_chats"
-  value       = [for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : v.site]
+  description = "Map of site values across all bot_channel_web_chats, keyed the same as var.bot_channel_web_chats"
+  value       = { for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : k => v.site }
 }
 
