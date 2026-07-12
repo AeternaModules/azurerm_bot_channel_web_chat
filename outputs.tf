3edad17870f25cedf5c90f985db859f9f0b4d394
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "bot_channel_web_chats_id" {
+  description = "Map of id values across all bot_channel_web_chats, keyed the same as var.bot_channel_web_chats"
+  value       = { for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : k => v.id }
+}
 output "bot_channel_web_chats_bot_name" {
   description = "Map of bot_name values across all bot_channel_web_chats, keyed the same as var.bot_channel_web_chats"
   value       = { for k, v in azurerm_bot_channel_web_chat.bot_channel_web_chats : k => v.bot_name }
